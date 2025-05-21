3e950ca1db7845e17119a5db2dce10308774caaa
Rupali Parse <parserupali@gmail.com>
2025-05-21 21:18:43 +0400

    Added final all test pass screenshot

--- string_calculator.rb
+++ string_calculator.rb
@@ -5,36 +5,36 @@ class StringCalculator
     return 0 unless input.match?(/\A[^a-zA-Z]*\z/)
     return (input.match?(/\A\d+\z/) ? input.to_i : nil) if input.length == 1
 
-    select_operands_from_input(input).sum
+    extract_valid_operands(input).sum
   end
 
-  def select_operands_from_input(input)
+  def extract_valid_operands(input)
     numbers = input
     delimiters = ["\n", "\,"]
 
-    delimiters, numbers = specific_delimeters(input) if input.start_with?("//")
+    delimiters, numbers = extract_custom_delimiters(input) if input.start_with?("//")
 
     operands = operands(numbers, Regexp.union(delimiters))
 
-    reject_invalid_operand(operands)
+    filter_negatives_and_large_numbers(operands)
   end
 
   def operands(numbers, delimiters_regex)
     numbers.split(delimiters_regex).map(&:to_i)
   end
 
-  def reject_invalid_operand(operands)
+  def filter_negatives_and_large_numbers(operands)
     negatives = operands.select(&:negative?)
     raise "Negative numbers not allowed: #{negatives.join(', ')}" if negatives.any?
 
     operands.reject { |n| n > 1000 }
   end
 
-  def specific_delimeters(input)
+  def extract_custom_delimiters(input)
     delimiter_part, numbers = input.split("\n", 2)
 
     delimiters = (delimiter_part.include?('[') && delimiter_part.include?(']')) ?
                    delimiter_part.scan(/\[(.+?)\]/).flatten : delimiter_part[/\/\/(.*)/, 1]
-    return delimiters, numbers
+    [delimiters, numbers]
   end
 end
